f3f8d427a1df1888627afefde5ae4eba25edfdd9
profslpa <profslpa@ulife.com.br>
2026-04-09 11:01:41 -0300

    pequeno update no repositorio de produto

--- SistemaPedidos/src/Repository/ProdutoRepositorio.java
+++ SistemaPedidos/src/Repository/ProdutoRepositorio.java
@@ -5,12 +5,31 @@ import Model.Produto;
 import java.util.ArrayList;
 
 public class ProdutoRepositorio {
+    public String resultado;
     int contadorId = 1;
-    private ArrayList<Produto> produtos = new ArrayList<>();
+    private ArrayList<Produto> estoque = new ArrayList<>();
 
     public void criarProduto(int id, String nome, double preco){
         Produto novoProduto = new Produto(id, nome, preco);
-        produtos.add(novoProduto);
+        estoque.add(novoProduto);
         contadorId++;
+    }
+
+    public void listarProdutos(){
+        if(estoque.isEmpty()){
+            System.out.println("Nenhum produto no estoque.");
+        } else {
+            for(Produto produto : estoque){
+                System.out.println(produto);
+            }
+        }
+    }
+
+    public void buscarProduto(String termo){
+        for(Produto produto : estoque){
+            if(produto.getNome().contains(termo)){
+                resultado = produto;
+            }
+        }
     }
 }
